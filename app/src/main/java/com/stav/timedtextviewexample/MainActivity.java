f572a686ceeb6674db4fc5ae48fc5ecedb4398e7
JStav <hiorelse@gmail.com>
2016-09-14 15:23:04 -0400

    Initial commit

---
+++ app/src/main/java/com/stav/timedtextviewexample/MainActivity.java
@@ -0,0 +1,58 @@
+package com.stav.timedtextviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.stav.timedtextview.util.TimeAgoTextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TimeAgoTextView tvTimeAgo = (TimeAgoTextView) findViewById(R.id.tv_timed);
+        Button btnStart = (Button) findViewById(R.id.btn_start_updates);
+        Button btnStop = (Button) findViewById(R.id.btn_stop_updates);
+        Button btnResetTime = (Button) findViewById(R.id.btn_reset_time);
+
+        if(btnStart != null) {
+            btnStart.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if(tvTimeAgo != null) {
+                        tvTimeAgo.startUpdates();
+                    }
+                }
+            });
+        }
+
+        if(btnStop != null) {
+            btnStop.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if(tvTimeAgo != null) {
+                        tvTimeAgo.stopUpdates();
+                    }
+                }
+            });
+        }
+
+        if(btnResetTime != null) {
+            btnResetTime.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if(tvTimeAgo != null) {
+                        tvTimeAgo.setTimestamp(System.currentTimeMillis());
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ timedtextview/src/main/java/com/stav/timedtextview/base/TextFormatter.java
@@ -0,0 +1,9 @@
+package com.stav.timedtextview.base;
+
+/**
+ * Formatter to let the TimedTextView know what to update the text with
+ * when it does update.
+ */
+public interface TextFormatter {
+    String format();
+}
